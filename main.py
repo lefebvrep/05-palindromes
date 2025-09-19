5e41700ab7d9031da71fb7bbfb4adde2da246f67
lefebvrep <82941164+lefebvrep@users.noreply.github.com>
2025-09-19 05:59:12 +0000

    version plus bas niveau

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 #### Fonction secondaire
 
 
-def ispalindrome(p):
+def ispalindrome(s):
 
-    s1 = p.replace(' ','').lower()
-    s2 = s1[::-1]
-    return p == s2 
-    #i = 0
-    #j = len(s) - 1
-    #while i < j:
-    #    while s[i] == ' ': i +=1
-    #    while s[j] == ' ': j -=1
-    #    if s[i].lower() != s[j].lower():
-    #        return False
-    ##    i += 1
-    #    j -= 1
-    # return True
+    #s1 = p.replace(' ','').lower()
+    #s2 = s1[::-1]
+    #return p == s2 
+    i = 0
+    j = len(s) - 1
+    while i < j:
+        while s[i] == ' ': i +=1
+        while s[j] == ' ': j -=1
+        if s[i].lower() != s[j].lower():
+            return False
+        i += 1
+        j -= 1
+    return True
 
 #### Fonction principale
 
