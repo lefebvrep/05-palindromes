377581f5697a7bdef1c4f7e0d237c9b69fcd5dff
lefebvrep <82941164+lefebvrep@users.noreply.github.com>
2025-09-19 05:23:04 +0000

    version simple du palindrome

--- main.py
+++ main.py
@@ -3,9 +3,19 @@
 
 def ispalindrome(p):
 
-    # votre code ici
-    
-    return False
+    s1 = p.replace(' ','').lower()
+    s2 = s1[::-1]
+    return p == s2 
+    #i = 0
+    #j = len(s) - 1
+    #while i < j:
+    #    while s[i] == ' ': i +=1
+    #    while s[j] == ' ': j -=1
+    #    if s[i].lower() != s[j].lower():
+    #        return False
+    ##    i += 1
+    #    j -= 1
+    # return True
 
 #### Fonction principale
 
